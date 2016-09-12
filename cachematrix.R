b4816b5a6265c963fc197bf7eb4e31c31e61bc5e
memresener <zczlmes@ucl.ac.uk>
2016-09-12 10:21:19 +0100

    cacheMatrix implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions are used to implement the solve() function for a matrix
+## makeCacheMatrix is used to create a CacheMatrix object with inbuilt cache for the result and getter/setters
+## cacheSolve checks if cached result is available, and reports the value of solve() or getresult()
 
-## Write a short comment describing this function
+## makeCacheMatrix
+## params: @x <invertible matrix>
+## creates object storing the matrix, it's inverse(if set) and the relevant getter/setter functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+     x <<- y
+     m <<- NULL
+  }
+  get <- function() x
+  setresult <- function(result) m <<- result
+  getresult <- function() m
+  list(set = set, get = get,
+       setresult = setresult,
+       getresult = getresult)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve
+##params: @x <cacheMatrix>
+##Attempts to check if the matrix has the inverse cached
+##If cache is not set, calculates inverse with solve() and sets cached result
+##If cache is set, returns cached value
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x) {
+    m <- x$getresult()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data)
+    x$setresult(m)
+    m
 }
+
